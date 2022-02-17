1bb9e19dc3e9c7134fceb615ed7f59a2d4792118
Pablo Alvarado-Moya <palvaradomoya@gmail.com>
2022-02-17 11:29:54 -0600

    Más comentarios y preparando algunos paquetes en el código base

--- lec04/probs.m
+++ lec04/probs.m
@@ -5,7 +5,9 @@
 ## En esta práctica se deberán calcular y graficar algunas
 ## probabilidades de letras en palabras de 5 letras en español.
 
-## Cargar todas las palabras de 5 letras como un cell array
+## Cargar todas las palabras de 5 letras como un cell array.
+## Ese archivo ha sido limpiado de caracteres extraños como espacios,
+## paréntesis, o la letra O.
 fid=fopen("palabras5ascii.txt");
 O=textscan(fid,'%s');
 fclose(fid);
@@ -15,9 +17,14 @@ fclose(fid);
 ## palabras en una sola línea, donde cada palabra ocupa 6 caracteres,
 ## siendo el último un espacio.
 S=[double(strjoin({O{1,1}{:}})) 32];
+## La ñ está codificada como N que no calza en la secuencia.  La vamos
+## a pasar al 123 que es '}' después de z
+S(S==78)=123;
+S=S-double('a');
 
 ## Esto transforma las palabras cada una en una fila de D.  Se recorta
 ## la última columna de espacios.
 D=reshape(S,6,length(S)/6)'(:,1:5);
 
+pkg load statistics;
 
