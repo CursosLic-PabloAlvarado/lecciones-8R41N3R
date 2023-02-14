0d8b27c8bcd6aee31b233ca2499f07c4e5af6e26
Brainer Borge <brainer1155@gmail.com>
2023-02-14 14:59:01 -0600

    .

--- lec03/linalg.m
+++ lec03/linalg.m
@@ -1,17 +1,22 @@
 #!/usr/bin/octave-cli
 
 ## Cargue los datos en el archivo 'escazu'.dat
-
+D=load("escazu.dat");
 
 ## Extraiga la columna de áreas y precios
-
-
+x=D(:,1);
+y=D(:,4);
 ## Extraiga min-max de áreas y precios
+minArea = min(x);
+minPrecio = min(y);
+maxArea = max(x);
+maxPrecio = max(y);
 
 
 ## Use las ecuaciones normales para calcular los parámetros de
 ## regresión lineal
-
+X = [ones(rows(x),1),x];
+theta = pinv(X'*X)*X'*y;
 
 ## Grafique los puntos originales y la recta de regresión lineal con
 ## al menos 100 puntos
